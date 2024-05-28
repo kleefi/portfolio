e6aafef68d75c779e3de0f1553784f2a0e31ca4f
kleefi <decharvi.charles@gmail.com>
2024-05-29 01:06:26 +0700

    dark mode feature

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function(){
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
-    if(window.pageYOffset>fixedNav){
+    if(window.scrollY>fixedNav){
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     }else{
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 }
 
@@ -19,3 +24,26 @@ hamburger.addEventListener('click', function(){
     navMenu.classList.toggle('hidden');
 
 })
+
+// klik diluar humberger
+window.addEventListener('click', function(e){
+    if(e.target != navMenu && e.target != hamburger){
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+// dark mode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click',function(){
+    darkToggle.checked ? (html.classList.add('dark'), localStorage.theme = 'dark') : (html.classList.remove('dark'), localStorage.theme = 'light');
+})
+
+// toggle mode
+        if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        darkToggle.checked=true
+        } else {
+        darkToggle.checked=false
+        }
