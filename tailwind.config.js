67a2f6097b7c7c17065f2a54bcb7aed559893297
kleefi <decharvi.charles@gmail.com>
2024-05-30 00:17:17 +0700

    prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
-  darkMode: 'class',
+  content: ["index.html"],
+  darkMode: "class",
   theme: {
-    container:{
-      center:true,
-      padding:'24px'
+    container: {
+      center: true,
+      padding: "24px",
     },
     extend: {
-      colors:{
-        primary:'#D77921',
-        dark:'#1e293b'
+      colors: {
+        primary: "#D77921",
+        dark: "#1e293b",
       },
-      screens:{
-        '2xl':'1320px',
+      screens: {
+        "2xl": "1320px",
       },
-      fontSize:{
-        'text-3xl':'1.875rem'
+      fontSize: {
+        "text-3xl": "1.875rem",
       },
     },
   },
   plugins: [],
-}
-
+};
